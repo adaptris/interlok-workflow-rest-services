cd2fe6444d7d3b3d6865bce3495f6d2135f59a0a
Aaron McGrath <aaron.mcgrath@adaptris.com>
2021-07-26 14:52:20 +0100

    INTERLOK-3781 Fixing up a bit of coverage.

--- interlok-rest-base/src/test/java/com/adaptris/rest/WorkflowServicesComponentTest.java
+++ interlok-rest-base/src/test/java/com/adaptris/rest/WorkflowServicesComponentTest.java
@@ -60,7 +60,7 @@ public class WorkflowServicesComponentTest {
 
   @Before
   public void setUp() throws Exception {
-    MockitoAnnotations.initMocks(this);
+    MockitoAnnotations.openMocks(this);
     
     message = DefaultMessageFactory.getDefaultInstance().newMessage();
     returnedMessage = DefaultMessageFactory.getDefaultInstance().newMessage();
